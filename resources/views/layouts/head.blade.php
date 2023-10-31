8b975b1fa3c9532abf22d654e72d284e014f06d6
ParaskevasLysikatos <vaslysalex@hotmail.gr>
2023-10-31 17:27:42 +0200

    jquery internal

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -3,5 +3,5 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Welcome to Shopping</title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script type="text/javascript" src="{{ URL::asset('jquery.js') }}"></script>
 </head>
